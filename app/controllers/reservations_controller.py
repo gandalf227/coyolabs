47cea175e55ea7ff85cf91e7e5535d09ff4d4ef8
Javier Alexis Herrera Castro <142291245+gandalf227@users.noreply.github.com>
2026-04-03 21:31:15 -0700

    Fix delivered parsing in admin ticket bulk update

--- app/controllers/reservations_controller.py
+++ app/controllers/reservations_controller.py
@@ -1012,6 +1012,7 @@ def admin_ticket_update_all(ticket_id: int):
         for i in range(len(item_ids)):
             try:
                 item_id = int(item_ids[i])
+                delivered = int(delivered_list[i] or 0)
                 returned = int(returned_list[i])
             except (ValueError, IndexError):
                 continue
